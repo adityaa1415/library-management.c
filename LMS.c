b82492a0287f17b61b17aef1f30ae2d83cf133c5
adityaa1415 <104295910+adityaa1415@users.noreply.github.com>
2022-06-29 17:42:10 +0530

    Update LMS.c

--- LMS.c
+++ LMS.c
@@ -1,106 +1,215 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-// Create Structure of Library
-struct library {
-	char book_name[20];
-	char author[20];
-	int pages;
-	float price;
-};
+struct books{
+    int id;
+    char bookName[50];
+    char authorName[50];
+    char date[12];
+}b;
+
+struct student{
+    int id;
+    char sName[50];
+    char sClass[50];
+    int sRoll;
+    char bookName[50];
+    char date[12];
+}s;
+
+FILE *fp;
+
+int main(){
+
+    int ch;
+
+    while(1){
+        system("cls");
+        printf("<== Library Management System ==>\n");
+        printf("1.Add Book\n");
+        printf("2.Books List\n");
+        printf("3.Remove Book\n");
+        printf("4.Issue Book\n");
+        printf("5.Issued Book List\n");
+        printf("0.Exit\n\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch(ch){
+        case 0:
+            exit(0);
+
+        case 1:
+            addBook();
+            break;
+
+        case 2:
+            booksList();
+            break;
+
+        case 3:
+            del();
+            break;
+
+        case 4:
+            issueBook();
+            break;
+
+        case 5:
+            issueList();
+            break;
+
+        default:
+            printf("Invalid Choice...\n\n");
+
+        }
+        printf("Press Any Key To Continue...");
+        getch();
+    }
+
+    return 0;
+}
 
 
-int main()
-{
-	// Create a instance
-	struct library lib[100];
+void addBook(){
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(b.date, myDate);
 
-	char ar_nm[30], bk_nm[30];
+    fp = fopen("books.txt", "ab");
 
-	// Keep the track of the number of books available in the library
-	int i, input, count;
+    printf("Enter book id: ");
+    scanf("%d", &b.id);
 
-	i = input = count = 0;
+    printf("Enter book name: ");
+    fflush(stdin);
+    gets(b.bookName);
 
-	// Iterate the loop
-	while (input != 5) {
+    printf("Enter author name: ");
+    fflush(stdin);
+    gets(b.authorName);
 
-		printf("\n\n********###### WELCOME TO E-LIBRARY #####********\n");
-		printf("\n\n1. Add book information\n2. Display book information\n");
-		printf("3. List all books of given author\n");
-		printf(
-			"4. List the count of books in the library\n");
-		printf("5. Exit");
+    printf("Book Added Successfully");
 
-		// Enter the book details
-		printf("\n\nEnter one of the above: ");
-		scanf("%d", &input);
+    fwrite(&b, sizeof(b), 1, fp);
+    fclose(fp);
+}
 
-		// Process the input
-		switch (input) {
 
-		// Add book
-		case 1:
+void booksList(){
 
-			printf("Enter book name = ");
-			scanf("%s", lib[i].book_name);
+    system("cls");
+    printf("<== Available Books ==>\n\n");
+    printf("%-10s %-30s %-20s %s\n\n", "Book id", "Book Name", "Author", "Date");
 
-			printf("Enter author name = ");
-			scanf("%s", lib[i].author);
+    fp = fopen("books.txt", "rb");
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        printf("%-10d %-30s %-20s %s\n", b.id, b.bookName, b.authorName, b.date);
+    }
 
-			printf("Enter pages = ");
-			scanf("%d", &lib[i].pages);
+    fclose(fp);
+}
 
-			printf("Enter price = ");
-			scanf("%f", &lib[i].price);
-			count++;
+void del(){
+    int id, f=0;
+    system("cls");
+    printf("<== Remove Books ==>\n\n");
+    printf("Enter Book id to remove: ");
+    scanf("%d", &id);
 
-			break;
+    FILE *ft;
 
-		// Print book information
-		case 2:
-			printf("you have entered the following information\n");
-			for (i = 0; i < count; i++) {
+    fp = fopen("books.txt", "rb");
+    ft = fopen("temp.txt", "wb");
 
-				printf("book name = %s",
-					lib[i].book_name);
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        if(id == b.id){
+            f=1;
+        }else{
+            fwrite(&b, sizeof(b), 1, ft);
+        }
+    }
 
-				printf("\t author name = %s",
-					lib[i].author);
+    if(f==1){
+        printf("\n\nDeleted Successfully.");
+    }else{
+        printf("\n\nRecord Not Found !");
+    }
 
-				printf("\t pages = %d",
-					lib[i].pages);
+    fclose(fp);
+    fclose(ft);
 
-				printf("\t price = %f",
-					lib[i].price);
-			}
-			break;
+    remove("books.txt");
+    rename("temp.txt", "books.txt");
 
-		// Take the author name as input
-		case 3:
-			printf("Enter author name : ");
-			scanf("%s", ar_nm);
-			for (i = 0; i < count; i++) {
+}
 
-				if (strcmp(ar_nm,
-						lib[i].author)
-					== 0)
-					printf("%s %s %d %f",
-						lib[i].book_name,
-						lib[i].author,
-						lib[i].pages,
-						lib[i].price);
-			}
-			break;
 
-		// Print total count
-		case 4:
-			printf("\n No. of books in Library : %d",count);
-			break;
-		case 5:
-			exit(0);
-		}
-	}
-	return 0;
+void issueBook(){
+
+    char myDate[12];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    strcpy(s.date, myDate);
+
+    int f=0;
+
+    system("cls");
+    printf("<== Issue Books ==>\n\n");
+
+    printf("Enter Book id to issue: ");
+    scanf("%d", &s.id);
+
+    //Check if we have book of given id
+    fp = fopen("books.txt", "rb");
+
+    while(fread(&b, sizeof(b), 1, fp) == 1){
+        if(b.id == s.id){
+            strcpy(s.bookName, b.bookName);
+            f=1;
+            break;
+        }
+    }
+
+    if(f==0){
+        printf("No book found with this id\n");
+        printf("Please try again...\n\n");
+        return;
+    }
+
+    fp = fopen("issue.txt", "ab");
+
+    printf("Enter Student Name: ");
+    fflush(stdin);
+    gets(s.sName);
+
+    printf("Enter Student Class: ");
+    fflush(stdin);
+    gets(s.sClass);
+
+    printf("Enter Student Roll: ");
+    scanf("%d", &s.sRoll);
+
+    printf("Book Issued Successfully\n\n");
+
+    fwrite(&s, sizeof(s), 1, fp);
+    fclose(fp);
+}
+
+void issueList(){
+    system("cls");
+    printf("<== Book Issue List ==>\n\n");
+
+    printf("%-10s %-30s %-20s %-10s %-30s %s\n\n", "S.id", "Name", "Class", "Roll", "Book Name", "Date");
+
+    fp = fopen("issue.txt", "rb");
+    while(fread(&s, sizeof(s), 1, fp) == 1){
+        printf("%-10d %-30s %-20s %-10d %-30s %s\n", s.id, s.sName, s.sClass, s.sRoll, s.bookName, s.date);
+    }
+
+    fclose(fp);
 }
